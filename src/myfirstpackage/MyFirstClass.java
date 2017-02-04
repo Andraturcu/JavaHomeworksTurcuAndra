d82abe946042c80dbcba3102cb74e52830a97f5a
Paul <Paul@PaulProconfort>
2017-02-04 18:50:39 +0200

    tema nr.1

---
+++ src/myfirstpackage/MyFirstClass.java
@@ -0,0 +1,20 @@
+/*
+ * File of my first class written in Java
+ */
+package myfirstpackage;
+
+/**
+ * My first Java Class
+ * @author Paul
+ */
+public class MyFirstClass {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       System.out.println("first code");
+    }
+    
+    
+}
